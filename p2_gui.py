114359e466b994a48dad659bbd98124693d252ac
blambrig <blambrig@ucsc.edu>
2016-01-18 01:53:37 -0800

    Major Commit, Basic Functionality mcts_vanilla implemented, however need to adjust selection function

--- p2_gui.py
+++ p2_gui.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from p2_game import create_game, State
 
-import random_bot as red_bot
-import rollout_bot as blue_bot
+import mcts_vanilla as red_bot
+import mcts_vanilla as blue_bot
 
 BOTS = {'red': red_bot, 'blue': blue_bot}
 
